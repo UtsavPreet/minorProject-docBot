7667f5aa8c69457ff13e54aa5fbcc3651a0ca8a3
UtsavPreet <utsavpreet@gmail.com>
2017-10-21 18:36:44 +0530

    Integrated ACtions on Google Library

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
+process.env.DEBUG = 'actions-on-google:*';
+const {
+    DialogflowApp
+} = require('actions-on-google');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.yourAction = functions.https.onRequest((request, response) => {
+    const app = new DialogflowApp({
+        request,
+        response
+    });
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
+
+    // Fulfill action business logic
+    function responseHandler(app) {
+        // Complete your fulfillment logic and send a response
+        app.tell('Hello, World!');
+    }
+
+    const actionMap = new Map();
+    actionMap.set('input.welcome', responseHandler);
+
+    app.handleRequest(actionMap);
+});
